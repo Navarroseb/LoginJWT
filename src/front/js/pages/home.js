32733ad3edbc57e6dd4dcab863836fbdcd732057
Navarroseb <sebastnavarro@gmail.com>
2022-05-04 21:32:53 +0000

    Installing Flask_JWT_Extended

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { Component, useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
@@ -6,21 +6,34 @@ import "../../styles/home.css";
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
+	/* handleSubmit = e => {
+		e.preventDefault();
+
+		const data = {
+			email: this.email,
+			password: this.password
+		}
+	}; */
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+			<div>
+				<form /* onSubmit={this.handleSubmit} */>
+					<input
+						type="email"
+						id="email"
+						placeholder="Email"
+						className="input1"
+					/>
+					<input
+						type="password"
+						id="password"
+						placeholder="Create a new password"
+						className="input2"
+					/>
+					<button type="submit">Register!</button>
+				</form>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
 		</div>
 	);
 };
