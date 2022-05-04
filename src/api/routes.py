d63d737c3d02bc413ea73a604fe546eb3f2ac1f0
Navarroseb <sebastnavarro@gmail.com>
2022-05-04 22:54:08 +0000

    create login, testing JWT

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,6 +1,7 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
@@ -10,10 +11,16 @@ from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
-
-@api.route('/token', methods=['POST'])
+@api.route("/token", methods=["POST"])
 def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
 
     
 
-    return jsonify(response_body), 200
+   
---
+++ src/front/js/pages/login.js
@@ -0,0 +1,39 @@
+import React, { Component, useContext } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+
+export const Home = () => {
+    const { store, actions } = useContext(Context);
+
+    /* handleSubmit = e => {
+        e.preventDefault();
+
+        const data = {
+            email: this.email,
+            password: this.password
+        }
+    }; */
+
+    return (
+        <div className="text-center mt-5">
+            <div>
+                <form /* onSubmit={this.handleSubmit} */>
+                    <input
+                        type="email"
+                        id="email"
+                        placeholder="Email"
+                        className="input1"
+                    />
+                    <input
+                        type="password"
+                        id="password"
+                        placeholder="Create a new password"
+                        className="input2"
+                    />
+                    <button type="submit">Register!</button>
+                </form>
+            </div>
+        </div>
+    );
+};
